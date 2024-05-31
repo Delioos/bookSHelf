99a530e333874e7caec32c201fc7bd0f7ab97f6c
Delioos <htz_j@protonmail.com>
2024-05-31 18:00:15 +0200

    refacto: main
    why: better understadning of used libs and code comprehnsion \n how: more methods extracts and clearer variables naming

--- src/main.rs
+++ src/main.rs
@@ -2,42 +2,69 @@ use std::process::Command;
 use walkdir::{DirEntry, WalkDir};
 use regex::Regex;
 
+//1 afficher toute la dir
+//2 ouvrir une nouvelle dir et l afficher
+//3 ouvrir un pdf et l'afficher
+// 4 afficher tout les pdf dans l'arborecence
+// 5 ajouter des notes sur un pdf
+// 6 ajouter des icons sympa dans l arbo / pdf et trunc les extension de fichier
+// 7 mettre en place un historique des livres lus pour faire un onglet fichier recents
+// 8 mettre en place un systeme de recherche de fichier tah fzf
+
 fn main() {
-    let pdfs = get_current_dir_pdfs();
-
-    // Attendre que l'utilisateur entre un index
-    println!("Entrez l'index du PDF que vous souhaitez ouvrir:");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).expect("Erreur lors de la lecture de l'entrée");
-    let index: usize = input.trim().parse::<usize>().expect("Entrée invalide") - 1; // Correction ici
-
-    // Ouvrir le PDF dans Firefox
-    let path_to_pdf = &pdfs[index].path().display().to_string();
-    open_pdf(path_to_pdf);
+    let dir :&str = "books";
+    display_dir(dir);
 }
 
-fn get_current_dir_pdfs() -> Vec<DirEntry> {
-    let re = Regex::new(r".*\.pdf$").unwrap();
-    let mut pdfs = Vec::new();
+fn display_dir(dir: &str) {
+    let path_size = dir.len() + 1; // remove the "root" dir + /
 
-    for entry in WalkDir::new(".").into_iter().filter_map(|e| e.ok()) {
-        if entry.file_type().is_file() && re.is_match(&entry.path().display().to_string()) {
-            pdfs.push(entry);
-        }
-    }
+    // get dirs
+    let dirs: Vec<DirEntry> = get_dirs(dir);
+    // truncate path
 
-    println!("Liste des PDF:");
-    for (index, pdf) in pdfs.clone().into_iter().enumerate() {
-        let path = pdf.path().display().to_string();
-        let trimmed_path = &path[2..];
-        println!("{}: {}", index + 1, trimmed_path);
-    }
-    pdfs
+    // get pdfs
+    let books: Vec<DirEntry> = get_books(dir);
+
+    // display
+    let mut i: u32 = 0;
+
+    // concat dirs and books
+    let mut all: Vec<DirEntry> = dirs.clone();
+    all.extend(books.clone());
+
+    all.iter()
+        .filter(|e| e.path().to_str().unwrap().to_string() != dir.to_string())
+        .for_each(|e| {
+        let path = e.path().to_str().unwrap();
+        let path = &path[path_size..];
+        println!("{} {}", i, path);
+        i += 1;
+    });
 }
 
-fn open_pdf(path_to_pdf: &String) -> () {
-    Command::new("firefox")
-        .arg(path_to_pdf)
-        .spawn()
-        .expect("Erreur lors de l'ouverture du PDF dans Firefox");
+fn get_dirs(dir: &str) -> Vec<DirEntry>{
+    WalkDir::new(dir)
+        .min_depth(0)
+        .max_depth(1)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_dir())
+        .collect()
+}
+
+fn get_books(dir: &str) -> Vec<DirEntry>{
+    WalkDir::new(dir)
+        .min_depth(0)
+        .max_depth(1)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file())
+        .filter(|e| is_book(e))
+        .collect()
+}
+
+fn is_book(e: &DirEntry) -> bool {
+    let re = Regex::new(r".pdf$  | .md$ | .epub$ |").unwrap();
+    re.is_match(e.path().to_str().unwrap())
 }
